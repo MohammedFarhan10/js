8f5e585e8e0443307568001029e3de0d8235cfcd
MohammedFarhan10 <mohammedfarhank6@gmail.com>
2024-06-05 18:22:35 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,54 @@
+// var names = 'amal'
+// console.log(names);
+// var empNames = "kavya"
+// console.log(empNames);
+// var studName = `anu`
+// console.log(studName);
+
+// console.log("One of my student is "+ studName);
+// console.log("One of my student is ", empNames);
+// console.log(`One of my student is ${names}`);
+
+// 2.typeof datatype
+// var n1 = 3;
+// var n2 = 3.433;
+// console.log(n1,n2);
+// console.log(typeof(n2));
+
+
+
+// 4.undefined datatype
+// let names;
+// console.log(names);
+
+// let lastname=null;
+// console.log(lastname);
+
+
+// const student={
+//     id: 123,
+//     name: 'manu',
+//     class:10
+// }
+// console.log(student);
+// console.log(student.name,student.id);
+
+
+// var student = ['john','abraham',2344,6789,true,null]
+// console.log(student);
+
+
+// function definition
+
+// function add(a,b){
+//     c=a+b
+//     console.log("sum of the numbers" +c);
+// }
+// add(5,7)
+
+
+function add2(a,b){
+        c=`sum of two numbers ${a+b}`
+        return c
+    }
+   console.log(add2(5,6));
